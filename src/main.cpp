043d6d72d2294a0029e0e39731461ca9c2897cf5
Arthur Negrão <arthurnegrao10s@gmail.com>
2026-03-22 22:18:16 -0300

    feat(main): Update the main function to run the app

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,10 @@
-#include <iostream>
-#include <string>
-
 #include <gtkmm/application.h>
 
-#include "ipc.hpp"
 #include "window.hpp"
 
+using json = nlohmann::json;
+
 int main(int argc, char *argv[]) {
-  const HyprTab::CHyprlandChannel channel;
-
-  std::string json = channel.request("j/clients");
-  std::cout << "Resposta do Hyprland:\n" << json << std::endl;
-
   auto app = Gtk::Application::create("org.hyprtab");
 
   app->make_window_and_run<HyprTab::HyprtabWindow>(argc, argv);
